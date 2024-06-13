3403f898ad851cf03cddd4ca891f0f7238a286b1
Tdsone <mail@timonschneider.de>
2024-06-13 22:13:39 +0200

    Deploy script now deploys comma separated list of modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,27 +2,30 @@
 
 # Function to deploy modules based on abbreviation
 deploy_module() {
-    case $1 in
-        is)
-            echo "Deploying infer_strandedness..."
-            modal deploy rnaseqpipe/modules/infer_strandedness/main.py
-            ;;
-        tg)
-            echo "Deploying trim-galore..."
-            modal deploy rnaseqpipe/modules/trim-galore.py
-            ;;
-        qc)
-            echo "Deploying fastqc..."
-            modal deploy rnaseqpipe/modules/fastqc.py
-            ;;
-        sa)
-            echo "Deploying staralign..."
-            modal deploy rnaseqpipe/modules/staralign.py
-            ;;
-        *)
-            echo "Unknown module: $1"
-            ;;
-    esac
+    IFS=',' read -ra MODULES <<< "$1"
+    for module in "${MODULES[@]}"; do
+        case $module in
+            is)
+                echo "Deploying infer_strandedness..."
+                modal deploy rnaseqpipe/modules/infer_strandedness/main.py
+                ;;
+            tg)
+                echo "Deploying trim-galore..."
+                modal deploy rnaseqpipe/modules/trim-galore.py
+                ;;
+            qc)
+                echo "Deploying fastqc..."
+                modal deploy rnaseqpipe/modules/fastqc.py
+                ;;
+            sa)
+                echo "Deploying staralign..."
+                modal deploy rnaseqpipe/modules/staralign.py
+                ;;
+            *)
+                echo "Unknown module: $module"
+                ;;
+        esac
+    done
 }
 
-deploy_module $1
+deploy_module "$1"
