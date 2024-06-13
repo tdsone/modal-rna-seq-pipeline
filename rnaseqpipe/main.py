f8b810794d2294eebeb37cf4b921a0e9c5d846e4
Tdsone <mail@timonschneider.de>
2024-06-13 15:04:35 +0200

    Resumable download

--- rnaseqpipe/main.py
+++ rnaseqpipe/main.py
@@ -1,8 +1,8 @@
-from modal import App, Image, Secret, method, build, enter, Cls, Volume
+from modal import App, Image, Secret, method, build, enter, Cls, Volume, Function
 from typing import Tuple, List
+from config import vol
 
 app = App("rna-seq")
-vol = Volume.from_name("rna-seq-data", create_if_missing=True)
 
 pipeline_img = Image.debian_slim().pip_install("azure-storage-blob", "tqdm")
 
@@ -14,12 +14,15 @@ pipeline_img = Image.debian_slim().pip_install("azure-storage-blob", "tqdm")
 )
 def run_pipeline(sample_id: Tuple[str, List[str]]):
     """
-    sample_id example paired: ("DRR023784", ["DRR023784_1.fastq", "DRR023784_2.fastq"])
-    sample_id example unpaired: ("DRR023784", ["DRR023784.fastq"])
+    Example Input:
+        sample_id example paired: ("DRR023784", ["DRR023784_1.fastq.gz", "DRR023784_2.fastq.gz"])
+        sample_id example unpaired: ("DRR023784", ["DRR023784.fastq.gz"])
     """
 
     # Create a pipeline ID
-    pipeline_id = f"pl-{sample_id[0]}"
+    plid = f"pl-{sample_id[0]}"
+
+    print(f"Running pipeline for {plid} with input {sample_id}...")
 
     import os
     from azure.storage.blob import BlobServiceClient
@@ -36,12 +39,70 @@ def run_pipeline(sample_id: Tuple[str, List[str]]):
     container_client = blob_service_client.get_container_client(container_name)
 
     # Download the blob(s)
-    for file in sample_id[1]:
-        blob_client = container_client.get_blob_client(file)
-        with open(f"data/{pipeline_id}/{file}", "wb") as f:
-            f.write(blob_client.download_blob().readall())
+    import os
 
-    # Run FastQC
+    print(f"{plid}: Downloading files...")
+
+    os.makedirs(f"/data/{plid}/reads", exist_ok=True)
+    for file in sample_id[1]:
+        if not os.path.exists(f"/data/{plid}/reads/{file}"):
+            blob_client = container_client.get_blob_client(file)
+            with open(f"/data/{plid}/reads/{file}", "wb") as f:
+                f.write(blob_client.download_blob().readall())
+        else:
+            print(f"File {file} already exists. Skipping download.")
+
+    vol.commit()
+
+    print(f"{plid}: Files downloaded successfully!")
+
+    """
+    In parallel, run: 
+    - Read Quality Check: FastQC
+    - Inferring Strandednes: fq and salmon
+    - Read Trimming: Trim Galore!
+    """
+
+    import subprocess
+    from concurrent.futures import ProcessPoolExecutor
+
+    fastqc = Function.lookup("rnaseq-fastqc", "fastqc")
+    infer_strandedness = Function.lookup("rnaseq-strandedness", "infer_strandedness")
+    trimgalore = Function.lookup("rnaseq-trimgalore", "trimgalore")
+
+    def run_fastqc():
+        return fastqc.remote(
+            plid=plid, read_files=[f"data/{plid}/reads/{name}" for name in sample_id[1]]
+        )
+
+    def run_infer_strandedness():
+        return infer_strandedness.remote(
+            plid=plid,
+            read_files=[f"data/{plid}/reads/{read_file}" for read_file in sample_id[1]],
+            assembly_name="R64-1-1",
+        )
+
+    def run_trimgalore():
+        return trimgalore.remote(
+            plid=plid, read_files=[f"data/{plid}/reads/{name}" for name in sample_id[1]]
+        )
+
+    def run_task(task):
+        try:
+            result = task()
+            return result
+        except Exception as e:
+            return False
+
+    # List of task functions
+    tasks = [run_fastqc, run_trimgalore, run_infer_strandedness]
+
+    # Run the tasks in parallel using subprocesses
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(run_task, task) for task in tasks]
+        results = [future.result() for future in futures]
+
+    print(f"{plid}: {all(results)} of 3 tasks completed successfully!")
 
 
 distr_img = Image.debian_slim().pip_install("azure-storage-blob")
@@ -66,7 +127,8 @@ def distribute_tasks():
     blob_list = container_client.list_blobs()
     blob_names = []
     for blob in blob_list:
-        blob_names.append(blob.name)
+        if blob.name.endswith(".fastq.gz"):
+            blob_names.append(blob.name)
 
     # Group files by prefix
     task_groups = {}
