58e6b21ce9e599830d4dae5ef7717f97946c0976
Tdsone <mail@timonschneider.de>
2024-06-13 15:05:04 +0200

    Deploy selected module in one script

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+modal deploy rnaseqpipe/modules/infer_strandedness/main.py
+modal deploy rnaseqpipe/modules/trim-galore.py
+modal deploy rnaseqpipe/modules/fastqc.py
+modal deploy rnaseqpipe/modules/staralign.py
